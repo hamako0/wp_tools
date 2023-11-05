7cca1559df2bf7a9fbea40c294d72c88a7e222fe
hamako0 <116480552+hamako0@users.noreply.github.com>
2023-11-05 15:54:07 +0900

    Update wp_upload.js

--- wp_upload/wp_upload.js
+++ wp_upload/wp_upload.js
@@ -1,12 +1,9 @@
-try {
-  (function () {
-    (($) => {
-      window.$ = $;
-      let timestamp = new Date().getTime();
-      let dir = "//x2627568.xaas3.jp/docs/bookmarklet/wp_upload";
-      jQuery("body").append("").append(``).append(``);
-    })(jQuery);
-  })();
-} catch (e) {
-  alert("エラー\n" + e);
-}
+(($) => {
+  window.$ = $;
+  let timestamp = new Date().getTime();
+  let dir = "//x2627568.xaas3.jp/docs/bookmarklet/wp_upload";
+  jQuery("body")
+    .append('<div id="root"></div>')
+    .append(`<link rel="stylesheet" href="${dir}/css/main.css?${timestamp}">`)
+    .append(`<script src="${dir}/js/main.js?${timestamp}"></script>`);
+})(jQuery);
