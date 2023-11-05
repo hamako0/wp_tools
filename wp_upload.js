1d7f6ec6b0917318b8784aeed4191d2465ed8b7e
hamako0 <116480552+hamako0@users.noreply.github.com>
2023-11-05 15:22:08 +0900

    Add files via upload

---
+++ wp_upload.js
@@ -0,0 +1,12 @@
+try {
+  (function () {
+    (($) => {
+      window.$ = $;
+      let timestamp = new Date().getTime();
+      let dir = "//x2627568.xaas3.jp/docs/bookmarklet/wp_upload";
+      jQuery("body").append("").append(``).append(``);
+    })(jQuery);
+  })();
+} catch (e) {
+  alert("エラー\n" + e);
+}
