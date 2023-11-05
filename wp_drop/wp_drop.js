3d98d9eb8280c8c7d965a20a041ddef7e83aaf1d
hamako0 <116480552+hamako0@users.noreply.github.com>
2023-11-05 15:26:32 +0900

    Add files via upload

---
+++ wp_drop/wp_drop.js
@@ -0,0 +1,12 @@
+try {
+  (function () {
+    (($) => {
+      window.$ = $;
+      let timestamp = new Date().getTime();
+      let dir = "//x2627568.xaas3.jp/docs/bookmarklet/wp_drop";
+      jQuery("body").append("").append(``).append(``);
+    })(jQuery);
+  })();
+} catch (e) {
+  alert("error\n" + e);
+}
