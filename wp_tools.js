79b414f90789af9b93d2964f4bfda36b75774b7b
hamako0 <116480552+hamako0@users.noreply.github.com>
2023-11-08 09:25:13 +0900

    Update wp_tools.js

--- wp_tools.js
+++ wp_tools.js
@@ -1,12 +1,1 @@
-javascript: (function () {
-  var o = { script: "wp_script_list", name: "yamaguchi" };
-  var d = document;
-  var s = d.createElement("script");
-  s.src =
-    "//x2627568.xaas3.jp/docs/userscript/other/marklets/" +
-    o.name +
-    "/?callback=script&trigger=" +
-    o.script;
-  d.body.appendChild(s);
-  d.body.removeChild(s);
-})();
+javascript:(function(){var o={script:"wp_script_list.js"};var d=document;var s=d.createElement("script");s.src="https://hamako0.github.io/wp_tools"+o.script;d.body.appendChild(s);d.body.removeChild(s)})()
